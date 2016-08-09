e5f3c7fb4304885b080a83e45fd5060086d62214
Jared Kipe <jared@hibou.io>
2016-08-09 09:35:10 -0700

    Add W4 "Additional Withholding", and "Non-Resident Alien" fields and calculations.  Contract level selection of FUTA rate (normal net is the default). Refactored tests for easier state testing.

--- l10n_us_hr_payroll/tests/test_us_payslip.py
+++ l10n_us_hr_payroll/tests/test_us_payslip.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from logging import getLogger
-from sys import float_info
+from sys import float_info as sys_float_info
 
 from openerp.tests import common
-from openerp.tools.float_utils import float_round
-
-DEBUG = False
-_logger = getLogger(__name__)
+from openerp.tools.float_utils import float_round as odoo_float_round
+from openerp.addons.l10n_us_hr_payroll.l10n_us_hr_payroll import USHrContract
 
 
 class TestUsPayslip(common.TransactionCase):
+    debug = False
+    _logger = getLogger(__name__)
+
+    float_info = sys_float_info
+
+    def float_round(self, value, digits):
+        return odoo_float_round(value, digits)
+
     _payroll_digits = -1
 
     @property
@@ -20,8 +26,8 @@ class TestUsPayslip(common.TransactionCase):
         return self._payroll_digits
 
     def _log(self, message):
-        if DEBUG:
-            _logger.warn(message)
+        if self.debug:
+            self._logger.warn(message)
             
     def _createEmployee(self):
         return self.env['hr.employee'].create({
@@ -32,7 +38,19 @@ class TestUsPayslip(common.TransactionCase):
             'name': 'Jared'
         })
 
-    def _createContract(self, employee, salary, schedule_pay='monthly', w4_allowances=0, w4_filing_status='single', external_wages=0.0,):
+    def _createContract(self, employee, salary,
+                        schedule_pay='monthly',
+                        w4_allowances=0,
+                        w4_filing_status='single',
+                        w4_is_nonresident_alien=False,
+                        w4_additional_withholding=0.0,
+                        external_wages=0.0,
+                        struct_id=False,
+                        futa_type=USHrContract.FUTA_TYPE_NORMAL,
+                        ):
+        if not struct_id:
+            struct_id = self.ref('l10n_us_hr_payroll.hr_payroll_salary_structure_us_employee')
+
         return self.env['hr.contract'].create({
             'date_start': '2016-01-01',
             'date_end': '2016-12-31',
@@ -40,12 +58,15 @@ class TestUsPayslip(common.TransactionCase):
             'wage': salary,
             'type_id': self.ref('hr_contract.hr_contract_type_emp'),
             'employee_id': employee.id,
-            'struct_id': self.ref('l10n_us_hr_payroll.hr_payroll_salary_structure_us_employee'),
+            'struct_id': struct_id,
             'working_hours': self.ref('resource.timesheet_group1'),
             'schedule_pay': schedule_pay,
             'w4_allowances': w4_allowances,
             'w4_filing_status': w4_filing_status,
+            'w4_is_nonresident_alien': w4_is_nonresident_alien,
+            'w4_additional_withholding': w4_additional_withholding,
             'external_wages': external_wages,
+            'futa_type': futa_type,
         })
 
     def _createPayslip(self, employee, date_from, date_to):
@@ -74,208 +95,3 @@ class TestUsPayslip(common.TransactionCase):
     def assertPayrollEqual(self, first, second):
         self.assertAlmostEqual(first, second, self.payroll_digits)
 
-    ###
-    #   2016 Taxes and Rates
-    ###
-
-    def test_2016_taxes(self):
-        # salary is high so that second payslip runs over max
-        # social security salary
-        salary = 80000.0;
-
-        ## tax rates
-        FICA_SS = -0.062
-        FICA_M = -0.0145
-        FUTA = -0.06
-        FICA_M_ADD = -0.009
-
-        ## tax maximums
-        FICA_SS_MAX_WAGE = 118500.0
-        FICA_M_MAX_WAGE = float_info.max
-        FICA_M_ADD_START_WAGE = 200000.0
-        FUTA_MAX_WAGE = 7000.0
-
-
-        employee = self._createEmployee()
-
-        contract = self._createContract(employee, salary)
-
-        self._log('2016 tax first payslip:')
-        payslip = self._createPayslip(employee, '2016-01-01', '2016-01-31')
-
-        payslip.compute_sheet()
-
-        cats = self._getCategories(payslip)
-
-        self.assertPayrollEqual(cats['FICA_EMP_SS_WAGES'], salary)
-        self.assertPayrollEqual(cats['FICA_EMP_SS'], cats['FICA_EMP_SS_WAGES'] * FICA_SS)
-        self.assertPayrollEqual(cats['FICA_EMP_M_WAGES'], salary)
-        self.assertPayrollEqual(cats['FICA_EMP_M'], cats['FICA_EMP_M_WAGES'] * FICA_M)
-        self.assertPayrollEqual(cats['FICA_EMP_M_ADD_WAGES'], 0.0)
-        self.assertPayrollEqual(cats['FICA_EMP_M_ADD'], 0.0)
-        self.assertPayrollEqual(cats['FICA_COMP_SS'], cats['FICA_EMP_SS'])
-        self.assertPayrollEqual(cats['FICA_COMP_M'], cats['FICA_EMP_M'])
-        self.assertPayrollEqual(cats['FUTA_WAGES'], FUTA_MAX_WAGE)
-        self.assertPayrollEqual(cats['FUTA'], cats['FUTA_WAGES'] * FUTA)
-
-        payslip.process_sheet()
-
-        # Make a new payslip, this one will have maximums for FICA Social Security Wages
-
-        remaining_ss_wages = FICA_SS_MAX_WAGE - salary if (FICA_SS_MAX_WAGE - 2*salary < salary) else salary
-        remaining_m_wages = FICA_M_MAX_WAGE - salary if (FICA_M_MAX_WAGE - 2*salary < salary) else salary
-
-        self._log('2016 tax second payslip:')
-        payslip = self._createPayslip(employee, '2016-02-01', '2016-02-29')  # 2016 is a leap year
-
-        payslip.compute_sheet()
-
-        cats = self._getCategories(payslip)
-
-        self.assertPayrollEqual(cats['FICA_EMP_SS_WAGES'], remaining_ss_wages)
-        self.assertPayrollEqual(cats['FICA_EMP_SS'], cats['FICA_EMP_SS_WAGES'] * FICA_SS)
-        self.assertPayrollEqual(cats['FICA_EMP_M_WAGES'], remaining_m_wages)
-        self.assertPayrollEqual(cats['FICA_EMP_M'], cats['FICA_EMP_M_WAGES'] * FICA_M)
-        self.assertPayrollEqual(cats['FICA_EMP_M_ADD_WAGES'], 0.0)
-        self.assertPayrollEqual(cats['FICA_EMP_M_ADD'], 0.0)
-        self.assertPayrollEqual(cats['FICA_COMP_SS'], cats['FICA_EMP_SS'])
-        self.assertPayrollEqual(cats['FICA_COMP_M'], cats['FICA_EMP_M'])
-        self.assertPayrollEqual(cats['FUTA_WAGES'], 0)
-        self.assertPayrollEqual(cats['FUTA'], 0)
-
-        payslip.process_sheet()
-
-        # Make a new payslip, this one will have reached Medicare Additional (employee only)
-
-        self._log('2016 tax third payslip:')
-        payslip = self._createPayslip(employee, '2016-03-01', '2016-03-31')
-
-        payslip.compute_sheet()
-
-        cats = self._getCategories(payslip)
-
-        self.assertPayrollEqual(cats['FICA_EMP_M_WAGES'], salary)
-        self.assertPayrollEqual(cats['FICA_EMP_M_ADD_WAGES'], FICA_M_ADD_START_WAGE - (salary * 2))  # aka 40k
-        self.assertPayrollEqual(cats['FICA_EMP_M_ADD'], cats['FICA_EMP_M_ADD_WAGES'] * FICA_M_ADD)
-
-        payslip.process_sheet()
-
-        # Make a new payslip, this one will have all salary as Medicare Additional
-
-        self._log('2016 tax fourth payslip:')
-        payslip = self._createPayslip(employee, '2016-04-01', '2016-04-30')
-
-        payslip.compute_sheet()
-
-        cats = self._getCategories(payslip)
-
-        self.assertPayrollEqual(cats['FICA_EMP_M_WAGES'], salary)
-        self.assertPayrollEqual(cats['FICA_EMP_M_ADD_WAGES'], salary)
-        self.assertPayrollEqual(cats['FICA_EMP_M_ADD'], cats['FICA_EMP_M_ADD_WAGES'] * FICA_M_ADD)
-
-        payslip.process_sheet()
-
-    def test_2016_fed_income_withholding_single(self):
-        salary = 6000.00
-        schedule_pay = 'monthly'
-        w4_allowances = 3
-        w4_allowance_amt = 337.50 * w4_allowances
-        adjusted_salary = salary - w4_allowance_amt  # should be 4987.50, but would work over a wide value for the rate
-        ###
-        # Single MONTHLY form Publication 15
-        expected_withholding = float_round(-(431.95 + ((adjusted_salary - 3325) * 0.25)), self.payroll_digits)
-
-        employee = self._createEmployee()
-        contract = self._createContract(employee, salary, schedule_pay, w4_allowances, 'single')
-
-        self._log('2016 fed income single payslip: adjusted_salary: ' + str(adjusted_salary))
-        payslip = self._createPayslip(employee, '2016-01-01', '2016-01-31')
-
-        payslip.compute_sheet()
-
-        cats = self._getCategories(payslip)
-
-        self.assertPayrollEqual(cats['FED_INC_WITHHOLD'], expected_withholding)
-
-    def test_2016_fed_income_withholding_married_as_single(self):
-        salary = 500.00
-        schedule_pay = 'weekly'
-        w4_allowances = 1
-        w4_allowance_amt = 77.90 * w4_allowances
-        adjusted_salary = salary - w4_allowance_amt  # should be 422.10, but would work over a wide value for the rate
-        ###
-        # Single MONTHLY form Publication 15
-        expected_withholding = float_round(-(17.90 + ((adjusted_salary - 222) * 0.15)), self.payroll_digits)
-
-        employee = self._createEmployee()
-        contract = self._createContract(employee, salary, schedule_pay, w4_allowances, 'married_as_single')
-
-        self._log('2016 fed income married_as_single payslip: adjusted_salary: ' + str(adjusted_salary))
-        payslip = self._createPayslip(employee, '2016-01-01', '2016-01-31')
-
-        payslip.compute_sheet()
-
-        cats = self._getCategories(payslip)
-
-        self.assertPayrollEqual(cats['FED_INC_WITHHOLD'], expected_withholding)
-
-    def test_2016_fed_income_withholding_married(self):
-        salary = 14000.00
-        schedule_pay = 'bi-weekly'
-        w4_allowances = 2
-        w4_allowance_amt = 155.80 * w4_allowances
-        adjusted_salary = salary - w4_allowance_amt  # should be 1368.84, but would work over a wide value for the rate
-        ###
-        # Single MONTHLY form Publication 15
-        expected_withholding = float_round(-(1992.05 + ((adjusted_salary - 9231) * 0.33)), self.payroll_digits)
-
-        employee = self._createEmployee()
-        contract = self._createContract(employee, salary, schedule_pay, w4_allowances, 'married')
-
-        self._log('2016 fed income married payslip: adjusted_salary: ' + str(adjusted_salary))
-        payslip = self._createPayslip(employee, '2016-01-01', '2016-01-31')
-
-        payslip.compute_sheet()
-
-        cats = self._getCategories(payslip)
-
-        self.assertPayrollEqual(cats['FED_INC_WITHHOLD'], expected_withholding)
-
-    def test_2016_taxes_with_external(self):
-        ## tax rates
-        FICA_SS = -0.062
-        FICA_M = -0.0145
-        FUTA = -0.06
-        FICA_M_ADD = -0.009
-
-        ## tax maximums
-        FICA_SS_MAX_WAGE = 118500.0
-        FICA_M_MAX_WAGE = float_info.max
-        FICA_M_ADD_START_WAGE = 200000.0
-        FUTA_MAX_WAGE = 7000.0
-
-        # social security salary
-        salary = FICA_M_ADD_START_WAGE
-        external_wages = 6000.0
-
-        employee = self._createEmployee()
-
-        contract = self._createContract(employee, salary, external_wages=external_wages)
-
-        self._log('2016 tax first payslip:')
-        payslip = self._createPayslip(employee, '2016-01-01', '2016-01-31')
-
-        payslip.compute_sheet()
-
-        cats = self._getCategories(payslip)
-
-        self.assertPayrollEqual(cats['FICA_EMP_SS_WAGES'], FICA_SS_MAX_WAGE - external_wages)
-        self.assertPayrollEqual(cats['FICA_EMP_SS'], cats['FICA_EMP_SS_WAGES'] * FICA_SS)
-        self.assertPayrollEqual(cats['FICA_EMP_M_WAGES'], salary)
-        self.assertPayrollEqual(cats['FICA_EMP_M'], cats['FICA_EMP_M_WAGES'] * FICA_M)
-        self.assertPayrollEqual(cats['FICA_EMP_M_ADD_WAGES'], external_wages)
-        self.assertPayrollEqual(cats['FICA_EMP_M_ADD'], cats['FICA_EMP_M_ADD_WAGES'] * FICA_M_ADD)
-        self.assertPayrollEqual(cats['FICA_COMP_SS'], cats['FICA_EMP_SS'])
-        self.assertPayrollEqual(cats['FICA_COMP_M'], cats['FICA_EMP_M'])
-        self.assertPayrollEqual(cats['FUTA_WAGES'], FUTA_MAX_WAGE - external_wages)
-        self.assertPayrollEqual(cats['FUTA'], cats['FUTA_WAGES'] * FUTA)
