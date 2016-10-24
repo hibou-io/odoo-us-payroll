aa574b88be6beddf50c6f29a5ab556f9315f6c15
Jared Kipe <jared@hibou.io>
2016-10-24 12:02:13 -0700

    Ohio Unemployment and Allowances implementation.

---
+++ l10n_us_oh_hr_payroll/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'USA - Ohio - Payroll',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'license': 'AGPL-3',
+    'category': 'Localization',
+    'depends': ['l10n_us_hr_payroll'],
+    'version': '0.9.0',
+    'description': """
+USA::Ohio Payroll Rules.
+========================
+
+    * Ohio Department of Revenue partner
+    * Contribution register for Ohio DoR Unemployment
+    * Contribution register for Ohio DoR Income Tax Withholding
+    * Contract level Ohio Withholding Allowance
+    * Company level Ohio Unemployment Rate
+    """,
+
+    'auto_install': False,
+    'website': 'https://hibou.io/',
+    'data':[
+        'hr_payroll_view.xml',
+        'hr_payroll_data.xml',
+    ],
+    'installable': True
+}
